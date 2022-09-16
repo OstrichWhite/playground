23e1b8ac9fa96999b03ced65643a785978da60cd
OstrichWhite <rahimcsestudies@gmail.com>
2022-09-16 14:42:54 +0530

    Marksheet problem

--- marksheet.js
+++ marksheet.js
@@ -1,21 +1,21 @@
-// const data=[
-//   {name:'rahim',rollno:1,subjects:{maths:40,science:78,language:98}},
-//   {name:'kabeer',rollno:2,subjects:{maths:35,science:35,language:35}},
-//   {name:'kishore',rollno:3,subjects:{maths:40,science:98,language:78}},
-//   {name:'umar',rollno:4,subjects:{maths:34,science:88,language:98}},
-// ]
-
 const data=[
-  {name:'Sriram',rollno:1,subjects:{language:'90',maths: '80',science: '33'}},
-  {name:'ashiq',rollno:1,subjects:{language:'90',maths: '80',science: '33'}},
-  {name:'Mani',rollno:1,subjects:{language:'60',maths: '70',science: '46'}},
-  {name:'Kishore',rollno:1,subjects:{language:'60',maths: '70',science: '46'}},
-  {name:'sai',rollno:1,subjects:{language:'70',maths: '70',science: '75'}},
-  {name:'abdul',rollno:1,subjects:{language:'60',maths: '70',science: '46'}},
-  {name:'kabeer',rollno:1,subjects:{language:'70',maths: '70',science: '75'}},
-  {name:'umar',rollno:1,subjects:{language:'70',maths: '70',science: '75'}},
-  {name:'nithis',rollno:1,subjects:{language:'35',maths: '35',science: '35'}}
+  {name:'rahim',rollno:1,subjects:{maths:40,science:78,language:98}},
+  {name:'kabeer',rollno:2,subjects:{maths:35,science:35,language:35}},
+  {name:'kishore',rollno:3,subjects:{maths:40,science:98,language:78}},
+  {name:'umar',rollno:4,subjects:{maths:34,science:88,language:98}},
 ]
+
+// const data=[
+//   {name:'Sriram',rollno:1,subjects:{language:'90',maths: '80',science: '33'}},
+//   {name:'ashiq',rollno:1,subjects:{language:'90',maths: '80',science: '33'}},
+//   {name:'Mani',rollno:1,subjects:{language:'60',maths: '70',science: '46'}},
+//   {name:'Kishore',rollno:1,subjects:{language:'60',maths: '70',science: '46'}},
+//   {name:'sai',rollno:1,subjects:{language:'70',maths: '70',science: '75'}},
+//   {name:'abdul',rollno:1,subjects:{language:'60',maths: '70',science: '46'}},
+//   {name:'kabeer',rollno:1,subjects:{language:'70',maths: '70',science: '75'}},
+//   {name:'umar',rollno:1,subjects:{language:'70',maths: '70',science: '75'}},
+//   {name:'nithis',rollno:1,subjects:{language:'35',maths: '35',science: '35'}}
+// ]
 
 const {collection}= require('@laufire/utils');
 
@@ -32,17 +32,11 @@ collection.map(data,(e)=>{
 let failStatistics = collection.filter(statistics,e=>e.test=='fail')
 collection.map(failStatistics,e=>e.rank='-')
 
-let passStatistics = collection.filter(statistics,e=>e.test!=='fail')//.sort(function (a, b) {return b.total- a.total});//sort the passed student marks
-passStatistics=collection.sort(passStatistics,((a, b)=> {return b.total- a.total}))
+let passStatistics = collection.filter(statistics,e=>e.test!=='fail')
+passStatistics=collection.sort(passStatistics,(a, b)=> b.total- a.total) //Sort by Rank
 
 let rank=1,check=0, count=1
 collection.map(passStatistics,(e,i)=>{ 
-    // if(check!=e.total){
-    //   check=e.total;
-    //   rank++
-    // }
-    // e.rank = rank;
-
     if(check!=e.total){
       check=e.total;
       rank=count
@@ -50,12 +44,10 @@ collection.map(passStatistics,(e,i)=>{
     e.rank=rank
     count++
   })
-  .sort(function (a, b) {return a.rank - b.rank}); //Sort by rank here to show in the table
 
-// const studentStatistics= passStatistics.concat(failStatistics)
-const studentStatistics=[]
+let studentStatistics=[]
 collection.combine(studentStatistics,passStatistics,failStatistics)
-
+// studentStatistics =collection.sort(studentStatistics,(a,b)=>a.rollno-b.rollno) //sort by rollno
 console.table(studentStatistics)
 console.log(`passed count ${passStatistics.length} failed count ${failStatistics.length}`)
 
