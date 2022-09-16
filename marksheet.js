0befdfcb75dd3411de6c5bc09042c35f93723b5f
OstrichWhite <rahimcsestudies@gmail.com>
2022-09-16 20:13:04 +0530

    Introduced displayMarksheet.

--- marksheet.js
+++ marksheet.js
@@ -1,13 +1,6 @@
 const { values,reduce,map,count,sort } = require('@laufire/utils/collection');
 const { index } = require('@laufire/utils/crunch');
 
-const students=[
-  {name:'rahim',rollno:1,subjects:{maths:40,science:78,language:98}},
-  {name:'kabeer',rollno:2,subjects:{maths:35,science:35,language:35}},
-  {name:'kishore',rollno:3,subjects:{maths:40,science:98,language:78}},
-  {name:'umar',rollno:4,subjects:{maths:34,science:88,language:98}},
-]
-
 const assignResult = (students)=>
   map(students,(student)=>{
     const {subjects} =student;
@@ -21,10 +14,11 @@ const assignResult = (students)=>
     };
   });
 
-const assignRank = (passedStudents)=>{
+const assignRank = (students)=>{
+  const { pass: passedStudents, fail: failedStudents } = index(students,['result']);
   const sortedStudents=sort(passedStudents,(a,b)=>b.total-a.total);
 
-  return reduce(sortedStudents,(acc,student,i)=>{ 
+  const { rankedStudents } = reduce(sortedStudents,(acc,student,i) => { 
     const {rank,check,count,rankedStudents} =acc;
     const {total} = student;
     const updatedRank=check!=total?count:rank;
@@ -37,16 +31,26 @@ const assignRank = (passedStudents)=>{
       rankedStudents:[...rankedStudents,{...student,rank:updatedRank}],
     };
   },{rank:1,check:0,count:1,rankedStudents:[]});
-}
-  
-  const generateMarksheet = (students)=>{
-  const studentResults= assignResult(students);
-  const {pass:passedStudents,fail:failedStudents}=index(studentResults,['result']);
 
-  const {rankedStudents} = assignRank(passedStudents);
-  
-  console.table(rankedStudents.concat(failedStudents));
-  console.log(`passed count ${count(passedStudents)} failed count ${count(failedStudents)}`);  
+  return [
+    ...rankedStudents,
+    ...failedStudents,
+  ];
 };
 
-generateMarksheet(students);
+const displayMarksheet = (students) => {
+  const { pass: passedStudents, fail: failedStudents } = index(students, ['result']);
+
+  console.table(students);
+  console.log(`passed count ${count(passedStudents)} failed count ${count(failedStudents)}`); 
+};
+  
+const generateMarksheet = (students) =>
+  displayMarksheet(assignRank(assignResult(students)));
+
+generateMarksheet([
+  {name:'rahim',rollno:1,subjects:{maths:40,science:78,language:98}},
+  {name:'kabeer',rollno:2,subjects:{maths:35,science:35,language:35}},
+  {name:'kishore',rollno:3,subjects:{maths:40,science:98,language:78}},
+  {name:'umar',rollno:4,subjects:{maths:34,science:88,language:98}},
+]);
