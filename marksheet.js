9e68961958866259e424a7cd4b18c1092438cc64
OstrichWhite <rahimcsestudies@gmail.com>
2022-09-16 19:32:51 +0530

    Introduced assignResult.

--- marksheet.js
+++ marksheet.js
@@ -1,4 +1,4 @@
-const { values,reduce,map,filter,combine,sort } = require('@laufire/utils/collection');
+const { values,reduce,map,filter,sort } = require('@laufire/utils/collection');
 
 const students=[
   {name:'rahim',rollno:1,subjects:{maths:40,science:78,language:98}},
@@ -7,34 +7,42 @@ const students=[
   {name:'umar',rollno:4,subjects:{maths:34,science:88,language:98}},
 ]
 
-const studentResults=map(students,(student)=>{
-  const {subjects} =student;
-  const total=reduce(subjects,(sum,mark)=>sum+mark,0)
-  const result=values(subjects).some((subject)=>subject<35)?'fail':'pass';
-  
-  return {
-    ...student,
-    total,
-    result,
-  };
-})
-
-const passedStudents=sort(filter(studentResults,e=>e.result==='pass'),(a,b)=>b.total-a.total)
-const failedStudents=filter(studentResults,e=>e.result==='fail');
-
-const {rankedStudents} = reduce(passedStudents,(acc,student,i)=>{ 
-  const {rank,check,count,rankedStudents} =acc;
-  const {total} = student;
-  const updatedRank=check!=total?count:rank;
-
+const assignResult = (students)=>
+  map(students,(student)=>{
+    const {subjects} =student;
+    const total=reduce(subjects,(sum,mark)=>sum+mark,0);
+    const result=values(subjects).some((subject)=>subject<35)?'fail':'pass';
+    
     return {
-      ...acc,
-      check:check!=total?total:check,
-      count:count+1,
-      rank:updatedRank,
-      rankedStudents:[...rankedStudents,{...student,rank:updatedRank}],
+      ...student,
+      total,
+      result,
     };
-  },{rank:1,check:0,count:1,rankedStudents:[]});
+  });
 
-console.table(rankedStudents.concat(failedStudents));
-console.log(`passed count ${passedStudents.length} failed count ${failedStudents.length}`);
+
+const generateMarksheet = (students)=>{
+  const studentResults= assignResult(students);
+  
+  const passedStudents=sort(filter(studentResults,e=>e.result==='pass'),(a,b)=>b.total-a.total)
+  const failedStudents=filter(studentResults,e=>e.result==='fail');
+  
+  const {rankedStudents} = reduce(passedStudents,(acc,student,i)=>{ 
+    const {rank,check,count,rankedStudents} =acc;
+    const {total} = student;
+    const updatedRank=check!=total?count:rank;
+  
+      return {
+        ...acc,
+        check:check!=total?total:check,
+        count:count+1,
+        rank:updatedRank,
+        rankedStudents:[...rankedStudents,{...student,rank:updatedRank}],
+      };
+    },{rank:1,check:0,count:1,rankedStudents:[]});
+  
+  console.table(rankedStudents.concat(failedStudents));
+  console.log(`passed count ${passedStudents.length} failed count ${failedStudents.length}`);  
+};
+
+generateMarksheet(students);
