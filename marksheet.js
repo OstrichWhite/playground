aa0a2480ee29e47a6c55a102a03a59fac361be22
OstrichWhite <rahimcsestudies@gmail.com>
2022-09-16 19:14:47 +0530

    Generated students results without mutation.

--- marksheet.js
+++ marksheet.js
@@ -1,39 +1,38 @@
-const data=[
+const { values,reduce,map,filter,combine,sort } = require('@laufire/utils/collection');
+
+const students=[
   {name:'rahim',rollno:1,subjects:{maths:40,science:78,language:98}},
   {name:'kabeer',rollno:2,subjects:{maths:35,science:35,language:35}},
   {name:'kishore',rollno:3,subjects:{maths:40,science:98,language:78}},
   {name:'umar',rollno:4,subjects:{maths:34,science:88,language:98}},
 ]
 
-const {collection}= require('@laufire/utils');
-
-let statistics=[];
-
-collection.map(data,(e)=>{
-  let total= Number(e.subjects.language)+Number(e.subjects.maths)+Number(e.subjects.science);
-  (e.subjects.language<35 || e.subjects.maths<35 || e.subjects.science<35)
-    ?statistics.push({ name: e.name, rollno: e.rollno, total: total, test: 'fail' })
-    :statistics.push({ name: e.name, rollno: e.rollno, total: total, test: 'pass' })
+const studentResults=map(students,(student)=>{
+  const {subjects} =student;
+  const total=reduce(subjects,(sum,mark)=>sum+mark,0)
+  const result=values(subjects).some((subject)=>subject<35)?'fail':'pass';
+  
+  return {
+    ...student,
+    total,
+    result,
+  };
 })
 
+const passStatistics=sort(filter(studentResults,e=>e.result==='pass'),(a,b)=>b.total-a.total)
+const failStatistics=filter(studentResults,e=>e.result==='fail');
 
-let failStatistics = collection.filter(statistics,e=>e.test=='fail')
-collection.map(failStatistics,e=>e.rank='-')
-
-let passStatistics = collection.filter(statistics,e=>e.test!=='fail')
-passStatistics=collection.sort(passStatistics,(a, b)=> b.total- a.total) //Sort by Rank
-
-let rank=1,check=0, count=1
-collection.map(passStatistics,(e,i)=>{ 
+let rank=1,check=0, count=1;
+map(passStatistics,(e,i)=>{ 
     if(check!=e.total){
       check=e.total;
-      rank=count
+      rank=count;
     }
-    e.rank=rank
-    count++
-  })
+    e.rank=rank;
+    count++;
+  });
 
-let studentStatistics=[]
-collection.combine(studentStatistics,passStatistics,failStatistics)
-console.table(studentStatistics)
-console.log(`passed count ${passStatistics.length} failed count ${failStatistics.length}`)
+let studentStatistics=[];
+combine(studentStatistics,passStatistics,failStatistics);
+console.table(studentStatistics);
+console.log(`passed count ${passStatistics.length} failed count ${failStatistics.length}`);
