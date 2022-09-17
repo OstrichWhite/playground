5c2ad79d3c1f6b1fb4ad01cc1df84b5ee0218c52
OstrichWhite <rahimcsestudies@gmail.com>
2022-09-18 05:15:14 +0530

    updateBalance refactored.

--- AccountProblem.js
+++ AccountProblem.js
@@ -54,28 +54,30 @@ const transactions = [
 const getBalance = (accountNo) => balances[accountNo];
 // const getBalance = (account) =>{ return { Name:account.name, Account:account.accountNo, Balance:balances[account.accountNo]} }
 
-const updateBalance = (transaction) => {
-  const updateBalanceConditions = {
-    withdrawal: ()=> balances[transaction.accountNo]-=transaction.amount,
-    deposit: ()=> balances[transaction.accountNo]+=transaction.amount,
-  };
-  return updateBalanceConditions[transaction.type]();
-};
-
 const displayBalance = () => accounts.map(e=>{ return { Name : e.name, Account : e.accountNo, Balance : getBalance(e.accountNo)} })  
 // const displayBalance = () => accounts.map(getBalance)  
 
-const displayAccount = (accounts,balances,transactions) => {
+const updateBalance = (data) => {
+  const{ balances,transactions } = data
+  transactions.map((transaction) => {
+    const updateBalanceConditions = {
+      withdrawal: ()=> balances[transaction.accountNo]-=transaction.amount,
+      deposit: ()=> balances[transaction.accountNo]+=transaction.amount,
+    };
+    return updateBalanceConditions[transaction.type]();
+  })
+}
+
+const displayAccountsReport = (data) => {
   console.log('Before Update Balance')
   console.table(displayBalance())
-  transactions.map(updateBalance)
+  updateBalance(data)
   console.log('After Update Balance')
   console.table(displayBalance())
-}
-
+};
 
 const main = () => {
-  displayAccount(accounts,balances,transactions)
-}
+  displayAccountsReport({accounts,balances,transactions});
+};
 
-main()
+main();
