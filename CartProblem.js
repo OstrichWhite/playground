4fbea568e40e9b7401921bdb952bddd41ec526ca
OstrichWhite <rahimcsestudies@gmail.com>
2022-09-19 11:57:38 +0530

    Introduced CartProblem

---
+++ CartProblem.js
@@ -0,0 +1,51 @@
+/* Data */
+const rates = {
+  Carrot: 10,
+  Apple: 200,
+  Guava: 50,
+};
+
+const discounts = {
+  // values are in percentages.
+  Apple: 10,
+};
+
+const taxes = {
+  // values are in percentages.
+  Carrot: 5,
+  Guava: 10,
+};
+const purchases = [
+  {
+    item: 'Carrot',
+    units: 20,
+  },
+  {
+    item: 'Apple',
+    units: 2,
+  },
+  {
+    item: 'Guava',
+    units: 1,
+  },
+];
+/* Functions */
+const getDiscountPercent = (productName) => (discounts[productName] || 0) /100 
+const getTaxPercent = (productName) => (taxes[productName] || 0) /100
+
+const getUnitPrice = (itemName) => {
+  const rate = rates[itemName]
+  const discount=rate*getDiscountPercent(itemName)
+  const tax=rate*getTaxPercent(itemName)
+  return rate-discount+tax
+}
+const getLineItemPrice = (lineItem) => lineItem.units*getUnitPrice(lineItem.item)
+const getSum = () => purchases.map(getLineItemPrice).reduce((a,b)=>a+b,0);
+
+// Do not change below this line.
+/* Main Function */
+const main = function main() {
+  console.log(getSum());
+}
+
+main();
